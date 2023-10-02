37c04769b02d89812d3a228b7097fd46ea11b149
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-10-03 04:07:15 +0530

    added features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+import Features from "@/components/Features";
+
 export default function Home() {
   return (
       <>
@@ -6,24 +8,26 @@ export default function Home() {
 
               <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
                   <div className="lg:col-span-3">
-                      <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl">
+                      <div className="flex flex-col items-center justify-center h-full">
+                         <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl">
                           ResQSync</h1>
-                      <p className="mt-3 text-lg text-gray-800 dark:text-gray-400">Keeping Loved Ones Close, Wherever They Roam. Safety First, Always Free!
-                      </p>
 
-                      <div className="mt-6">
-                          <a className="group inline-block bg-gray-900/[.05] hover:bg-gray-400/[.1] border border-gray-900/[.05] p-1 pl-4 rounded-full shadow-md" href="../figma.html">
-                              <p className="mr-2 inline-block text-black text-sm">
+                        <p className="mt-3 text-lg text-gray-600 text-center dark:text-gray-600">Keeping Loved Ones Close, Wherever They Roam. Safety First, Always Free!
+                        </p>
+
+                        <div className="mt-6">
+                              <a className="group inline-block bg-gray-900/[.05] hover:bg-gray-400/[.1] border border-gray-900/[.05] p-1 pl-4 rounded-full shadow-md" href="../figma.html">
+                                  <p className="mr-2 inline-block text-black text-sm">
                                   Download
-                              </p>
-                              <span className="group-hover:bg-gray-400/[.1] py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-900/[.075] font-semibold text-black text-sm">
-                                <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                    <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-                                </svg>
-                              </span>
-                          </a>
+                                </p>
+                                <span className="group-hover:bg-gray-400/[.1] py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-900/[.075] font-semibold text-black text-sm">
+                                    <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                        <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
                       </div>
-
                   </div>
 
 
@@ -36,6 +40,8 @@ export default function Home() {
               </div>
 
           </div>
+
+          <Features/>
 
 
       </>
---
+++ src/components/Features.tsx
@@ -0,0 +1,67 @@
+export default function Features(){
+    return(<>
+
+        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-20 mx-auto">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
+
+                <div className="text-center">
+                    <div className="flex justify-center items-center w-12 h-12 bg-gray-50 border border-gray-300 rounded-full mx-auto ">
+                        <svg className="w-5 h-5 text-gray-600 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                        </svg>
+                    </div>
+                    <div className="mt-3">
+                        <h3 className="text-lg font-semibold text-gray-800">Responsive</h3>
+                        <p className="mt-1 text-gray-600">Responsive, and mobile-first project on the web</p>
+                    </div>
+                </div>
+
+
+                <div className="text-center">
+                    <div className="flex justify-center items-center w-12 h-12 bg-gray-50 border border-gray-300 rounded-full mx-auto ">
+                        <svg className="w-5 h-5 text-gray-600 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M9.465 10H12a2 2 0 1 1 0 4H9.465c.34-.588.535-1.271.535-2 0-.729-.195-1.412-.535-2z"/>
+                            <path d="M6 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 1a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm.535-10a3.975 3.975 0 0 1-.409-1H4a1 1 0 0 1 0-2h2.126c.091-.355.23-.69.41-1H4a2 2 0 1 0 0 4h2.535z"/>
+                            <path d="M14 4a4 4 0 1 1-8 0 4 4 0 0 1 8 0z"/>
+                        </svg>
+                    </div>
+                    <div className="mt-3">
+                        <h3 className="text-lg font-semibold text-gray-800">Customizable</h3>
+                        <p className="mt-1 text-gray-600">Components are easily customized and extendable</p>
+                    </div>
+                </div>
+
+
+
+                <div className="text-center">
+                    <div className="flex justify-center items-center w-12 h-12 bg-gray-50 border border-gray-300 rounded-full mx-auto">
+                        <svg className="w-5 h-5 text-gray-600 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                            <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                        </svg>
+                    </div>
+                    <div className="mt-3">
+                        <h3 className="text-lg font-semibold text-gray-800">Documentation</h3>
+                        <p className="mt-1 text-gray-600">Every component and plugin is well documented</p>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <div className="flex justify-center items-center w-12 h-12 bg-gray-50 border border-gray-300 rounded-full mx-auto">
+                        <svg className="w-5 h-5 text-gray-600 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                            <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                        </svg>
+                    </div>
+                    <div className="mt-3">
+                        <h3 className="text-lg font-semibold text-gray-800">24/7 Support</h3>
+                        <p className="mt-1 text-gray-600 ">Contact us 24 hours a day, 7 days a week</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </>);
+}
