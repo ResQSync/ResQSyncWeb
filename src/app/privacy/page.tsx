6796be358b5364177365410df674ec615a774a10
Shruti Gitte <shrutigitte@gmail.com>
2023-10-13 18:12:57 +0530

    complete privacy policy page
    this is the complete privacy policy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,46 @@
+import Features from "@/components/Features";
+export default function privacy() {
+    return(
+        <>
+            <div>
+                <p className={"my-14 px-40 text-5xl font-bold"}>Privacy Policy</p>
+            </div>
+            <div className={"px-40"}>
+                <h4 className={"text-xl font-medium"}>Privacy Policy for ResQSync App</h4>
+                <p className={"my-10"}>Last Updated:15 October 2023</p>
+            </div>
+            <div className={"px-40"}>
+                <p> Welcome to ResQSync! ResQSync is an <span className={"font-bold"}>Open Source</span> mobile application developed with a strong
+                    commitment to user privacy and data security. We believe in <span className={"font-bold"}>Transparency</span> and have created this Privacy Policy to help you understand how we collect, use, and safeguard your personal information. By using ResQSync,
+                    you agree to the terms outlined in this Privacy Policy. <br/> <br/>
+                    <span className={"font-semibold text-xl"}>1.Information we Collect</span> <br/><br/>
+
+                    1.1. Location Data: ResQSync may request access to your device's location information to provide features such as location sharing and emergency services <span className={"font-semibold"}>(SOS)</span> . Your location data is collected only when you explicitly enable location sharing or trigger an SOS alert. We do not track your location continuously, and your location data is shared only with the individuals or authorities you specify during an SOS alert.
+                    <br/> <br/>
+                    1.2. Contact Access: To enhance the emergency response process, ResQSync may request access to your contact list to help you quickly notify and share your location with your trusted contacts in case of an emergency. We do not use this information for any purpose other than assisting you during emergency situations.
+                    <br/><br/>
+                    1.3. Usage Information: ResQSync may collect non-personal, aggregated data about how the app is used. This data helps us improve the app's performance and user experience. This information does not personally identify you and is not shared with third parties. <br/> <br/>
+
+                    <span className={"font-semibold text-xl"}>2. How we use your information</span> <br/><br/>
+                    2.1. Location Data: Your location data is used to provide real-time location sharing and to send SOS alerts to specified contacts or authorities when you activate the SOS feature. <br/><br/>
+                    2.2. Contact Access: Contact information is used to quickly inform your trusted contacts when an SOS alert is triggered. <br/><br/>
+                    2.3. Usage Information: We use usage information to analyze and improve the ResQSync app, its features, and its overall performance. This data is used for internal purposes only and is not shared with third parties. <br/><br/>
+                    <span className={"font-semibold text-xl"}>3. Data Security</span> <br/><br/>
+                    ResQSync takes data security seriously. We implement industry-standard security measures to protect your data from unauthorized access or disclosure. Our commitment to data security includes encryption, secure storage, and regular security assessments to ensure your information is safe. <br/><br/>
+                    <span className={"font-semibold text-xl"}>4. Sharing your Data</span> <br/><br/>
+                    We will not sell, rent, or share your personal information with third parties for marketing or advertising purposes. Your location data and contact information are shared only during SOS alerts with the specified individuals or authorities you have chosen to share it with. <br/><br/>
+                    <span className={"font-semibold text-xl"}>5. Open Source</span> <br/><br/>
+                    ResQSync is an open-source project, and the codebase is publicly available. We believe in transparency and open-source principles, which promote visibility and community review, ensuring that our privacy standards are met. <br/><br/>
+                    <span className={"font-semibold text-xl"}>6. Changes to this Privacy Policy</span> <br/><br/>
+                    We may update this Privacy Policy from time to time to reflect changes in the app or legal requirements. When we do make changes, we will post the updated Privacy Policy on the ResQSync website and notify you within the app. Your continued use of ResQSync will indicate your acceptance of the updated Privacy Policy. <br/><br/>
+                    <span className={"font-semibold text-xl"}>7. Contact Us</span> <br/><br/>
+                    If you have any questions or concerns about our Privacy Policy or how we handle your data, please feel free to reach out to us at <a href={"mailto:resqsync@gmail.com" } className={"text-blue-600"}>resqsync@gmail.com</a>. Your feedback and inquiries are important to us, and we are here to assist you. <br/><br/><br/>
+                    <span className={"text-xl"}>By using ResQSync, you acknowledge and consent to the practices described in this Privacy Policy. Your privacy and data security are paramount to us, and our commitment to transparency ensures that you can trust us with your information. Thank you for using ResQSync!</span> <br/><br/><br/>
+
+
+
+                </p>
+            </div>
+        </>
+    )
+}
